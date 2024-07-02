9bf586ee9942b935b1e82d299de48428c6104514
“sreelatha” <sreelatha.vallabhaneni@gmail.com>
2024-07-02 23:19:47 +0200

    created allRecipes component

---
+++ src/screens/AllRecipes.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AllRecipes() {
+  return (
+    <View>
+      <Text>AllRecipes</Text>
+    </View>
+  )
+}
