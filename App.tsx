42ecef02bb6983b4791690fca3535f327ccb502b
“sreelatha” <sreelatha.vallabhaneni@gmail.com>
2024-07-05 23:56:59 +0200

    deleted commented and unused code

--- App.tsx
+++ App.tsx
@@ -8,13 +8,8 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
 
 import {
