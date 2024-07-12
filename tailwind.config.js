83a924476c8e4f4aa9af15f196d286e0a392e549
“sreelatha” <sreelatha.vallabhaneni@gmail.com>
2024-07-12 23:49:55 +0200

    added new colors and fontFamily to tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        regularBrown: "#996B4D",
+        blackBrown: "#1C120D",
+        orange: "#ED7D2B",
+        creamWhite: "#F2EDE8",
+      },
+      fontFamily: {
+        'serif': ['Libre Baskerville', 'serif'],
+        'sans': ["AvenirLTPro-Book", 'sans-serif'],
+        'sans-b': ['AvenirLTPro-BookOblique', 'AvenirLTPro-Book', 'sans-serif'],
+        'com': ['commuters-sans', 'helvetica', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
